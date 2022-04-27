bf62709e9434fa45b7fbec9796936c9e09a34225
Shirwa331 <83422683+Shirwa331@users.noreply.github.com>
2022-04-26 21:28:08 -0700

    added chat page fragments, view model, and commented out the chat page and icon.

---
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/chat/ChatFragment.java
@@ -0,0 +1,23 @@
+package edu.uw.tcss450.Team4.TCSS450Project.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.Team4.TCSS450Project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
