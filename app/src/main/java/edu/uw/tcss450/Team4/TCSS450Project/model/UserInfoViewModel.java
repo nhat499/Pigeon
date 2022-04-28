7ed7c0de2fb0f4f387b3d863436cc94b3be37c1a
Eugene Oh <eugeneoh911@Gmail.com>
2022-04-27 21:18:03 -0700

    Added partial internet functionality for the registration fragment.

---
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/model/UserInfoViewModel.java
@@ -0,0 +1,48 @@
+package edu.uw.tcss450.Team4.TCSS450Project.model;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class UserInfoViewModel extends ViewModel {
+
+    private final String mEmail;
+    private final String mJwt;
+
+    private UserInfoViewModel(String email, String jwt) {
+        mEmail = email;
+        mJwt = jwt;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public String getmJwt() {
+        return mJwt;
+    }
+
+    public static class UserInfoViewModelFactory implements ViewModelProvider.Factory {
+
+        private final String email;
+        private final String jwt;
+
+        public UserInfoViewModelFactory(String email, String jwt) {
+            this.email = email;
+            this.jwt = jwt;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass == UserInfoViewModel.class) {
+                return (T) new UserInfoViewModel(email, jwt);
+            }
+            throw new IllegalArgumentException(
+                    "Argument must be: " + UserInfoViewModel.class);
+        }
+    }
+
+
+}
+
