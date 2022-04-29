6099a3d6e069905f5746cb3c5e329926e37fd249
AndreLarson <dre.larson7@gmail.com>
2022-04-29 12:47:05 -0700

    added home fragment to nav bar

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_chat, R.id.navigation_contacts, R.id.navigation_weather)
+                R.id.navigation_home, R.id.navigation_chat, R.id.navigation_contacts, R.id.navigation_weather)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
