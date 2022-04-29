b735ec6a8a94c1c7fbc4d3be9359047af1769e89
Eugene Oh <eugeneoh911@Gmail.com>
2022-04-28 21:23:00 -0700

    Added RegistrationViewModel

---
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/registration/RegistrationViewModel.java
@@ -0,0 +1,89 @@
+package edu.uw.tcss450.Team4.TCSS450Project.ui.registration;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+public class RegistrationViewModel extends AndroidViewModel {
+    private MutableLiveData<JSONObject> mResponse;
+
+    public RegistrationViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset())
+                    .replace('\"', '\'');
+            try {
+                JSONObject response = new JSONObject();
+                response.put("code", error.networkResponse.statusCode);
+                response.put("data", new JSONObject(data));
+                mResponse.setValue(response);
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
+//
+//    public void connect(final String first,
+//                        final String last,
+//                        final String email,
+//                        final String password) {
+//        String url = "https://cfb3-tcss450-labs-2021sp.herokuapp.com/auth";
+//        JSONObject body = new JSONObject();
+//        try {
+//            body.put("first", first);
+//            body.put("last", last);
+//            body.put("email", email);
+//            body.put("password", password);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        Request request = new JsonObjectRequest(
+//                Request.Method.POST,
+//                url,
+//                body,
+//                mResponse::setValue,
+//                this::handleError);
+//        request.setRetryPolicy(new DefaultRetryPolicy(
+//                10_000,
+//                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+//                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+//        //Instantiate the RequestQueue and add the request to the queue
+//        Volley.newRequestQueue(getApplication().getApplicationContext())
+//                .add(request);
+//    }
+}
