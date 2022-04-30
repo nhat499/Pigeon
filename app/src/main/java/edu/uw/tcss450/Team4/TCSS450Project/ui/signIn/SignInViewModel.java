7d3294ca82abd022725874ab3f8eb262d9d4468f
Eugene Oh <eugeneoh911@Gmail.com>
2022-04-30 13:47:33 -0700

    Added web service functionality to registration and sign-in.

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/signIn/SignInViewModel.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/signIn/SignInViewModel.java
@@ -1,6 +1,7 @@
-package edu.uw.tcss450.Team4.TCSS450Project.ui.registration;
+package edu.uw.tcss450.Team4.TCSS450Project.ui.signIn;
 
 import android.app.Application;
+import android.util.Base64;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -13,17 +14,22 @@ import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
-public class RegistrationViewModel extends AndroidViewModel {
+
+import edu.uw.tcss450.Team4.TCSS450Project.io.RequestQueueSingleton;
+
+public class SignInViewModel extends AndroidViewModel {
+
     private MutableLiveData<JSONObject> mResponse;
 
-    public RegistrationViewModel(@NonNull Application application) {
+    public SignInViewModel(@NonNull Application application) {
         super(application);
         mResponse = new MutableLiveData<>();
         mResponse.setValue(new JSONObject());
@@ -58,32 +64,32 @@ public class RegistrationViewModel extends AndroidViewModel {
         }
     }
 
-    public void connect(final String first,
-                        final String last,
-                        final String email,
-                        final String password) {
+    public void connect(final String email, final String password) {
         String url = "https://team-4-tcss-450-web-service.herokuapp.com/auth";
-        JSONObject body = new JSONObject();
-        try {
-            body.put("first", first);
-            body.put("last", last);
-            body.put("email", email);
-            body.put("password", password);
-        } catch (JSONException e) {
-            e.printStackTrace();
+    Request request = new JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null, //no body for this get request
+            mResponse::setValue,
+            this::handleError) {
+        @Override
+        public Map<String, String> getHeaders() {
+            Map<String, String> headers = new HashMap<>();
+            // add headers <key,value>
+            String credentials = email + ":" + password;
+            String auth = "Basic "
+                    + Base64.encodeToString(credentials.getBytes(),
+                    Base64.NO_WRAP);
+            headers.put("Authorization", auth);
+            return headers;
         }
-        Request request = new JsonObjectRequest(
-                Request.Method.POST,
-                url,
-                body,
-                mResponse::setValue,
-                this::handleError);
-        request.setRetryPolicy(new DefaultRetryPolicy(
-                10_000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+    };
+     request.setRetryPolicy(new DefaultRetryPolicy(
+     10_000,
+     DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+     DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Instantiate the RequestQueue and add the request to the queue
-        Volley.newRequestQueue(getApplication().getApplicationContext())
-                .add(request);
-    }
+     RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+}
 }
