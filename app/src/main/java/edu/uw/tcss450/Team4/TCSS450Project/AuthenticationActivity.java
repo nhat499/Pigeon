ee1efe437ce2241c06aef4e221bf3ba855854a7d
AndreLarson <dre.larson7@gmail.com>
2022-05-02 14:59:36 -0700

    added dark mode support

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/AuthenticationActivity.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/AuthenticationActivity.java
@@ -2,8 +2,11 @@ package edu.uw.tcss450.Team4.TCSS450Project;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class AuthenticationActivity extends AppCompatActivity {
     @Override
@@ -12,4 +15,22 @@ public class AuthenticationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         Log.d("onCreate", "onCreate: " + savedInstanceState);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_dark_mode) {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
@@ -1,11 +1,14 @@
 package edu.uw.tcss450.Team4.TCSS450Project;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,7 +35,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_dark_mode) {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
