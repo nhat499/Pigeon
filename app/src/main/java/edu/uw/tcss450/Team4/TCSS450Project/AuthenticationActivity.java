d1f0a951d7724c8e1e1176bf740febbce78ef93b
AndreLarson <dre.larson7@gmail.com>
2022-05-03 20:08:37 -0700

    added extra color themes and javadoc to activity files

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/AuthenticationActivity.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/AuthenticationActivity.java
@@ -1,13 +1,14 @@
 package edu.uw.tcss450.Team4.TCSS450Project;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+/**
+ *
+ * @author Team4
+ * @version May 2022
+ */
 public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +20,11 @@ public class AuthenticationActivity extends AppCompatActivity {
         toggleDarkMode(isChecked);
     }
 
+    /**
+     * Toggles between dark mode and light mode
+     *
+     * @param isChecked is true when the menu item action_dark_mode is checked and false otherwise
+     */
     private void toggleDarkMode(boolean isChecked) {
         if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
