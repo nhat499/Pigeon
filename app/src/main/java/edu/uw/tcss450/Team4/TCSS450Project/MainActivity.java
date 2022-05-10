80ac654850ed46fe046906aa4057f16b1da1b1bf
Eugene Oh <eugeneoh911@Gmail.com>
2022-05-10 15:01:17 -0700

    Fixed "token not valid" and added changed the text color.

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
 
         new ViewModelProvider(this,
-                new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
+                new UserInfoViewModel.UserInfoViewModelFactory(args.getJwt(),args.getEmail())
         ).get(UserInfoViewModel.class);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
