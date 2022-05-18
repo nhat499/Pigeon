75fc0d550ca9e89c1c74b4c36e1bb8a8de335acf
Nhat Trang <nhat499@uw.edu>
2022-05-17 22:00:09 -0700

    somework on landing functionality

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/homeLanding/HomeLandingFragment.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/homeLanding/HomeLandingFragment.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.uw.tcss450.Team4.TCSS450Project.R;
 import edu.uw.tcss450.Team4.TCSS450Project.model.UserInfoViewModel;
+import edu.uw.tcss450.Team4.TCSS450Project.databinding.FragmentHomeLandingBinding;
 
 /**
  * Class to define the fragment lifecycle for the HomeLanding Fragment
@@ -21,11 +28,33 @@ import edu.uw.tcss450.Team4.TCSS450Project.model.UserInfoViewModel;
  * @version May 2022
  */
 public class HomeLandingFragment extends Fragment {
+    private FragmentHomeLandingBinding mBinding;
+    private HomeLandingViewModel mHomeLandModel;
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        HomeLandingFragmentArgs  args = HomeLandingFragmentArgs
+                .fromBundle(getActivity().getIntent().getExtras());
+
+        Log.e("test respond", "onCreateView: " + args.getJwt());
+        mHomeLandModel = new ViewModelProvider(getActivity())
+                .get(HomeLandingViewModel.class);
+        mHomeLandModel.connect(args.getJwt(), args.getEmail()); // should be swap once fix
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //try{
+            //Log.e("test respond", "onCreateView: " +
+                //mHomeLandModel.mResponse; // tbd
+//        } catch (JSONException e) {
+//            Log.e("test respond", "onCreateView: " + e);
+//        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_landing, container, false);
     }
@@ -35,5 +64,20 @@ public class HomeLandingFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         UserInfoViewModel model = new ViewModelProvider(getActivity())
                 .get(UserInfoViewModel.class);
+
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+
+        } else {
+
+        }
     }
 }
--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/homeLanding/HomeLandingViewModel.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/homeLanding/HomeLandingViewModel.java
@@ -1,6 +1,26 @@
 package edu.uw.tcss450.Team4.TCSS450Project.ui.homeLanding;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import edu.uw.tcss450.Team4.TCSS450Project.io.RequestQueueSingleton;
 
 /**
  * View model for the HomeLanding Fragment
@@ -8,7 +28,63 @@ import androidx.lifecycle.ViewModel;
  * @author team4
  * @version May 2022
  */
-public class HomeLandingViewModel extends ViewModel {
+public class HomeLandingViewModel extends AndroidViewModel {
+    public MutableLiveData<JSONObject> mResponse;
+
+    public HomeLandingViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    public void connect(final String email, String jwt) {
+        String url = "https://team-4-tcss-450-web-service.herokuapp.com/contact/" + email;
+        Request request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                mResponse::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+
+        //code here will run
+    }
 
 
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset());
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "code:" + error.networkResponse.statusCode +
+                        ", data:" + data +
+                        "}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
 }
