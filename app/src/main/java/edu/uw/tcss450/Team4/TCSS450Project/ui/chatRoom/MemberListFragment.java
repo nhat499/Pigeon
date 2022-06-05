bd43f58ce98af99d50ffb4ab618119bf728a2d1e
Eugene Oh <eugeneoh911@Gmail.com>
2022-06-04 20:14:33 -0700

    added title for member fragment to nav bar

--- app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/chatRoom/MemberListFragment.java
+++ app/src/main/java/edu/uw/tcss450/Team4/TCSS450Project/ui/chatRoom/MemberListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -44,6 +45,8 @@ public class MemberListFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Users in Chat");
+
         FragmentMemberListBinding binding = FragmentMemberListBinding.bind(getView());
 
         MemberListFragmentArgs args = MemberListFragmentArgs.fromBundle(getArguments());
